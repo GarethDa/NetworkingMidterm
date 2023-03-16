b318857260029dc94e2e8e8aa5cc75c80e7b83ee
Haider Ali <Haiderali360@gmail.com>
2023-03-16 16:50:25 -0400

    TCP for Position
    But it needs to be for CHAT!
    Whoops

--- MidtermServer/MidtermServer/Program.cs
+++ MidtermServer/MidtermServer/Program.cs
@@ -11,33 +11,82 @@ namespace MidtermServer
 {
     class Program
     {
-        public static void StartServer()
+        Socket server;
+        byte[] buffer = new byte[512];
+        float[] pos;
+
+        List<Socket> connectedClients = new List<Socket>();
+
+        void StartServer() //Initializes the server to the localhost address and begins waiting for connections.
         {
             //Setup our server  
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             IPEndPoint serverEP = new IPEndPoint(ip, 8888);
 
-            Socket server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
             server.Bind(serverEP);
-            server.Listen(1);
+            //Start listening for connection requests.
+            server.Listen(8);
 
             // Accept connections 
             Console.WriteLine("Waiting for connections...");
 
-            Socket client = server.Accept();
-            Console.WriteLine("Client connected!");
-            IPEndPoint clientEP = (IPEndPoint)client.RemoteEndPoint;
-            Console.WriteLine("Client: {0}  Port: {1}", clientEP.Address, clientEP.Port);
-
-            byte[] msg = Encoding.ASCII.GetBytes("This is my first TCP server!!!!! Welcome to INFR3830");
-
-            // Sending data to connected client 
-            client.Send(msg);
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
+            server.BeginAccept(new AsyncCallback(AcceptCallback), null);
         }
 
-        public static int Main(String[] args)
+        void AcceptCallback(IAsyncResult result)
+        {
+            Socket client = server.EndAccept(result);
+            connectedClients.Add(client);
+            Console.WriteLine("Client connected! IP: {0}", client.RemoteEndPoint.ToString());
+
+            client.BeginReceive(buffer, 0, buffer.Length, 0, new AsyncCallback(ReceiveCallback), client);
+        }
+
+        void ReceiveCallback(IAsyncResult result)
+        {
+            Socket client = (Socket)result.AsyncState;
+            int rec = client.EndReceive(result);
+
+            byte[] data = new byte[rec];
+            Array.Copy(buffer, data, rec);
+            string msg = Encoding.ASCII.GetString(data);
+
+            if(msg == "quit")
+            {
+                connectedClients.Remove(client);
+                client.Close();
+                return;
+            }
+
+            pos = new float[rec / 4];
+            Buffer.BlockCopy(buffer, 0, pos, 0, rec);
+
+            //client.BeginSend(buffer, 0, buffer.Length, 0, new AsyncCallback(SendCallback), client);
+            Console.WriteLine("Received X:" + pos[0] + " Y:" + pos[1] + " Z:" + pos[2]);
+
+            byte[] send = new byte[pos.Length * sizeof(float)];
+            Buffer.BlockCopy(pos, 0, send, 0, send.Length);
+
+            foreach (Socket connectedClient in connectedClients)
+            {
+                if (connectedClient == client)
+                {
+                    continue;
+                }
+                connectedClient.BeginSend(send, 0, send.Length, 0, new AsyncCallback(SendCallback), connectedClient);
+            }
+
+            client.BeginReceive(buffer, 0, buffer.Length, 0, new AsyncCallback(ReceiveCallback), client);
+        }
+
+        private static void SendCallback(IAsyncResult result)
+        {
+            Socket socket = (Socket)result.AsyncState;
+            socket.EndSend(result);
+        }
+
+        public int Main(String[] args)
         {
             StartServer();
             Console.ReadKey();
--- MidtermSimpleClient/MidtermSimpleClient/Program.cs
+++ MidtermSimpleClient/MidtermSimpleClient/Program.cs
@@ -1,42 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace MidtermSimpleClient
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+namespace AsyncClient
 {
-    internal class Program
+    class Program
     {
-        public class AsyncServer
+        private static Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static byte[] buffer = new byte[512];
+        private static byte[] bpos;
+        private static float[] pos;
+
+
+        private static void ReceiveCallback(IAsyncResult results)
         {
-            private static byte[] buffer = new byte[512];
-            private static Socket server;
+            Socket socket = (Socket)results.AsyncState;
+            int rec = socket.EndReceive(results);
+            //lec06
+            pos = new float[rec / 4];
+            Buffer.BlockCopy(buffer, 0, pos, 0, rec);
+            Console.WriteLine("Client recv X:" + pos[0] + " Y:" + pos[1] + " Z:" +
+pos[2]);
+            //lec05
+            //byte[] data = new byte[rec];
+            //Array.Copy(buffer, data, rec);
+            //String msg = Encoding.ASCII.GetString(data);
+            //Console.WriteLine("Recv: " + msg);
+            socket.BeginReceive(buffer, 0, buffer.Length, 0,
+                new AsyncCallback(ReceiveCallback), socket);
+        }
+        private static void SyncSend()
+        {
+            int c = 0;
+            //lec06
+            float x, y, z = 0;
 
-            private static byte[] outBuffer = new byte[512];
-            private static string outMsg = "";
-
-            public static int Main(String[] args)
+            while (true)
             {
-                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //lec06
+                x = y = ++z;
+                pos = new float[] { x, y, z };
+                bpos = new byte[pos.Length * 4];
+                Buffer.BlockCopy(pos, 0, bpos, 0, bpos.Length);
+                //lec05
+                //byte[] buffer = Encoding.ASCII.GetBytes(c.ToString());
 
-                IPAddress ip = IPAddress.Parse("127.0.0.1");
+                //lec05
+                //client.Send(buffer);
 
-                server.Bind(new IPEndPoint(ip, 8888));
-
-                server.Listen(8);
-
-                server.BeginAccept(new AsyncCallback(AcceptCallback), null);
-
-                Console.Read();
+                client.Send(bpos);
+                c++;
+                Thread.Sleep(1000);
             }
-
-            private static void AcceptCallback(IAsyncResult result)
-            {
-                
-            }
+        }
+        static void Main(string[] args)
+        {
+            client.Connect(IPAddress.Parse("3.83.34.7"), 8888);
+            Console.WriteLine("Connected to server!!!");
+            client.BeginReceive(buffer, 0, buffer.Length, 0, new AsyncCallback(ReceiveCallback), client);
+            SyncSend();
+            Console.Read();
         }
     }
 }
